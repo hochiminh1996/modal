f695a2cb2f1ab12c5e281976069a1a736727ce54
hochiminh1996 <ambrsp@hotmail.com>
2023-03-13 16:14:13 -0300

    Imagens  do modal

--- modal/js/arquivo.js
+++ modal/js/arquivo.js
@@ -54,8 +54,15 @@ function informacoes(modal) {
     const btn_close = document.querySelector("#closeModal")
 
     const h1 = document.createElement("h1");
-    const img = document.createElement("img");
+    const img = document.createElement("img");//img da luly
     const p = document.createElement("p");
+    const div = document.createElement("div");
+    const img2 = document.createElement("img");
+    const img3 = document.createElement("img");
+    const img4 = document.createElement("img");
+    const img5 = document.createElement("img");
+    const img6 = document.createElement("img");
+
 
 
     h1.textContent = "Sobre mim";
@@ -64,10 +71,39 @@ function informacoes(modal) {
     img.src = "img/img.jpg";
     p.setAttribute("class", "paragrafo")
     p.textContent = "Meu nome é Felippe Marques, sou estudante de Análise e Desenvolvimento de Sistemas (ADS) no Instituto Federal de São Paulo (IFSP). Tenho interesse em desenvolvimento de software e gosto de usar a tecnologia para resolver problemas e criar soluções inovadoras. No meu tempo livre, gosto de assistir séries, ler livros de ficção científica e geopolítica. Estou sempre procurando por novas oportunidades de aprendizado.";
+    div.setAttribute("id", "div_img");//div
+    img2.src= "img/js.png";
+    img2.setAttribute("class", "img-techs");
+    img2.title = "JAVASCRIPT";
+
+    img3.src= "img/java.png";
+    img3.setAttribute("class", "img-techs");
+    img3.title = "JAVA";
+
+    img4.src= "img/servidor-sql.png";
+    img4.setAttribute("class", "img-techs");
+    img4.title = "SQL";
+
+    img5.src= "img/html-5.png";
+    img5.setAttribute("class", "img-techs");
+    img5.title = "HTML5";
+
+    img6.src= "img/css-3.png";
+    img6.setAttribute("class", "img-techs");
+    img6.title = "CSS3";
+
+
 
     div_info.appendChild(h1);
     div_info.appendChild(img)
     div_info.appendChild(p);
+    div_info.insertBefore(div, p.nextSibling);
+    div.appendChild(img2)
+    div.appendChild(img3)
+    div.appendChild(img4)
+    div.appendChild(img5);
+    div.appendChild(img6);
+
 
 }
 
